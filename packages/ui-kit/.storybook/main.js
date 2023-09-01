c9732f3596e92c7e4a5189af416221f1377c5169
avelascl <avelascl@nttdata.com>
2023-09-01 11:21:19 +0200

    chore(ui-kit): add @storybook/addon-a11y

--- packages/ui-kit/.storybook/main.js
+++ packages/ui-kit/.storybook/main.js
@@ -11,7 +11,12 @@ function getAbsolutePath(value) {
 /** @type { import('@storybook/html-webpack5').StorybookConfig } */
 const config = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [getAbsolutePath('@storybook/addon-links'), getAbsolutePath('@storybook/addon-essentials'), getAbsolutePath('@storybook/addon-interactions')],
+  addons: [
+    getAbsolutePath('@storybook/addon-links'),
+    getAbsolutePath('@storybook/addon-essentials'),
+    getAbsolutePath('@storybook/addon-interactions'),
+    getAbsolutePath('@storybook/addon-a11y'),
+  ],
   framework: {
     name: getAbsolutePath('@storybook/html-webpack5'),
     options: {},
