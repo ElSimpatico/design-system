2f348473030b1a22eed8dd9d5a70de4781783b6a
avelascl <avelascl@nttdata.com>
2023-09-04 13:09:27 +0200

    build(ui-kit): storybook compiler swc

--- packages/ui-kit/.storybook/main.js
+++ packages/ui-kit/.storybook/main.js
@@ -5,21 +5,25 @@ import { join, dirname } from 'path';
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
 function getAbsolutePath(value) {
-  return dirname(require.resolve(join(value, 'package.json')));
+    return dirname(require.resolve(join(value, 'package.json')));
 }
 
 /** @type { import('@storybook/html-webpack5').StorybookConfig } */
 const config = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [
-    getAbsolutePath('@storybook/addon-links'),
-    getAbsolutePath('@storybook/addon-essentials'),
-    getAbsolutePath('@storybook/addon-interactions'),
-    getAbsolutePath('@storybook/addon-a11y'),
-  ],
-  framework: {
-    name: getAbsolutePath('@storybook/html-webpack5'),
-    options: {},
-  },
+    stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+    addons: [
+        getAbsolutePath('@storybook/addon-links'),
+        getAbsolutePath('@storybook/addon-essentials'),
+        getAbsolutePath('@storybook/addon-interactions'),
+        getAbsolutePath('@storybook/addon-a11y'),
+    ],
+    framework: {
+        name: getAbsolutePath('@storybook/html-webpack5'),
+        options: {
+            builder: {
+                useSWC: true,
+            },
+        },
+    },
 };
 export default config;
