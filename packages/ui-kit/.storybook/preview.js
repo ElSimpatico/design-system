611d700963008808698e6647bf7e93254b532e72
avelascl <avelascl@nttdata.com>
2023-09-04 13:10:55 +0200

    styles(ui-kit): sass arquitecture 7 + 1

--- packages/ui-kit/.storybook/preview.js
+++ packages/ui-kit/.storybook/preview.js
@@ -1,18 +1,19 @@
 import { defineCustomElements } from '../dist/esm/loader';
+import '../dist/ui-kit/ui-kit.css';
 
 defineCustomElements();
 
 /** @type { import('@storybook/html').Preview } */
 const preview = {
-  parameters: {
-    actions: { argTypesRegex: '^on[A-Z].*' },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/,
-      },
+    parameters: {
+        actions: { argTypesRegex: '^on[A-Z].*' },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/,
+            },
+        },
     },
-  },
 };
 
 export default preview;
--- packages/ui-kit/stencil.config.ts
+++ packages/ui-kit/stencil.config.ts
@@ -1,31 +1,42 @@
 import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
 import { reactOutputTarget } from '@stencil/react-output-target';
+import { resolve } from 'path';
 
 export const config: Config = {
-  namespace: 'ui-kit',
-  outputTargets: [
-    {
-      type: 'dist',
-      esmLoaderPath: '../loader',
+    namespace: 'ui-kit',
+    outputTargets: [
+        {
+            type: 'dist',
+            esmLoaderPath: '../loader',
+        },
+        {
+            type: 'dist-custom-elements',
+        },
+        {
+            type: 'docs-readme',
+        },
+        {
+            type: 'www',
+            serviceWorker: null, // disable service workers
+        },
+        reactOutputTarget({
+            componentCorePackage: 'ui-kit',
+            proxiesFile:
+                '../ui-kit-react/lib/components/stencil-generated/index.ts',
+        }),
+    ],
+    globalStyle: resolve(__dirname, 'src/scss/main.scss'),
+
+    plugins: [
+        sass({
+            injectGlobalPaths: [
+                'src/scss/abstracts/_mixins.scss',
+                'src/scss/abstracts/_variables.scss',
+            ],
+        }),
+    ],
+    testing: {
+        browserHeadless: 'new',
     },
-    {
-      type: 'dist-custom-elements',
-    },
-    {
-      type: 'docs-readme',
-    },
-    {
-      type: 'www',
-      serviceWorker: null, // disable service workers
-    },
-    reactOutputTarget({
-      componentCorePackage: 'ui-kit',
-      proxiesFile: '../ui-kit-react/lib/components/stencil-generated/index.ts',
-    }),
-  ],
-  plugins: [sass()],
-  testing: {
-    browserHeadless: 'new',
-  },
 };
